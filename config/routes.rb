81d25ef7a97a46bd2d57b1e565685bfca0d626b6
AjaAdams <ajajhendrix@gmail.com>
2021-01-31 17:23:09 -0600

    adds product routes and seed data

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/products" => "products#index"
+    post "/products" => "products#create"
+    get "/products/:id" => "products#show"
+    patch "/products/:id" => "products#update"
+    delete "/products/:id" => "products#destroy"
+
   end
 end
